607375b4587dcb25ac52f8603c9cda53fb282b8f
Vadym <vad261275@gmail.com>
2017-08-10 13:26:11 +0300

    Delete stringToArray.php

--- stringToArray.php
+++
@@ -1,9 +0,0 @@
-<?php
-function strToArr ($str) {
-    $count = strlen($str);    
-    $arr = array ();    
-    for ($i = 0; $i < $count; $i++) {
-        $arr[] = $str[$i];
-    }
-    return $arr;
-}
